3512cc86e7286a97e10701b9f5080dd45d7ca8fe
truongaxin123 <truongaxin123@gmail.com>
2020-08-11 22:50:20 +0700

    edit models.py

--- CuXinShop/theblog/models.py
+++ CuXinShop/theblog/models.py
@@ -1,21 +1,28 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+from unidecode import unidecode
 
 
 class Tag(models.Model):
     tag_name = models.CharField(max_length=50)
+    tag_full_name = models.CharField(max_length=100, default='Fullname is none')
     pub_date = models.DateTimeField('Public date')
     description = models.TextField()
 
     def __str__(self):
         return self.tag_name
 
+    def get_raw_name(self):
+        return unidecode(self.tag_full_name)
+
 
 class Article(models.Model):
 
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Public date')
-    content = models.TextField()
+    content = RichTextUploadingField(blank=False)
 
     def __str__(self):
         return self.title
+
