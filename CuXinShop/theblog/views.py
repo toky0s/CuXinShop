347999e5a7f787ebb43137d5fb4ee84647c5d155
truongaxin123 <truongaxin123@gmail.com>
2020-08-11 22:48:52 +0700

    edit view

--- CuXinShop/theblog/views.py
+++ CuXinShop/theblog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.http import Http404
+from .models import Article, Tag
+from django.views.generic import DetailView, ListView
 
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article.html'
 
 
 def detail_article(request, id_article):
@@ -18,5 +26,15 @@ def list_article(request):
     return  HttpResponse('Đây là nơi hiển thị các bài viết mới nhất')
 
 
-def list_tag(requests):
+def list_tag(request):
     return HttpResponse('Đây là nơi hiển thị các tag')
+
+
+def list_article_with_tag(request, tag_name: str):
+    if len(Tag.objects.all().filter(tag_name=tag_name)) == 0:
+        return Http404('ko tim thay trang nay')
+    else:
+        tag = Tag.objects.all().filter(tag_name=tag_name)[0]
+        articles = tag.article_set.all()
+
+    return render(request, 'tag.html', context={'tag': tag,'articles':articles})
