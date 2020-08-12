2548624183673dcf3b81e8a756dd3c011218f064
truongaxin123 <truongaxin123@gmail.com>
2020-08-12 23:11:52 +0700

    edit urlpattern

--- CuXinShop/theblog/urls.py
+++ CuXinShop/theblog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'theblog'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('detail_article/<int:pk>', views.ArticleDetailView.as_view(), name='detail_article'),
-    path('detail_tag/<str:tag_name>', views.detail_tag, name='detail_tag'),
+    path('detail_article/<str:slug>/', views.detail_article, name='detail_article'),
+    path('detail_tag/<str:tag_name>/', views.detail_tag, name='detail_tag'),
     path('list_article/', views.list_article, name='list_article'),
     path('list_tag/', views.list_tag, name='list_tag'),
-    path('list_article/with_tag/<str:tag_name>', views.list_article_with_tag, name='list_article_with_tag')
+    path('list_article/with_tag/<str:tag_name>/', views.list_article_with_tag, name='list_article_with_tag')
 ]
