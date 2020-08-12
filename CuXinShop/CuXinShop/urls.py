45dc14705a576f74d9c5f686e5dcf9cfee71cdab
truongaxin123 <truongaxin123@gmail.com>
2020-08-12 23:12:03 +0700

    add namespace

--- CuXinShop/CuXinShop/urls.py
+++ CuXinShop/CuXinShop/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('theblog.urls')),
+    path('', include('theblog.urls', namespace='theblog')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
