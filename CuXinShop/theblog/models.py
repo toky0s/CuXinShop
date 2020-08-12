33abe8fd72f58a860a0bd9a0bb20ec36a29241cb
truongaxin123 <truongaxin123@gmail.com>
2020-08-12 23:12:18 +0700

    edit model add slug, view

--- CuXinShop/theblog/models.py
+++ CuXinShop/theblog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from ckeditor_uploader.fields import RichTextUploadingField
 from unidecode import unidecode
+from datetime import datetime
 
 
 class Tag(models.Model):
@@ -20,9 +22,16 @@ class Article(models.Model):
 
     tags = models.ManyToManyField(Tag)
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('Public date')
+    pub_date = models.DateTimeField('Public date', auto_now_add=True)
     content = RichTextUploadingField(blank=False)
+    view = models.IntegerField(default=0)
+    last_modify = models.DateTimeField(null=True, auto_now_add=True)
+    slug = models.SlugField(max_length=200, default='this-is-slug')
 
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(unidecode(self.title))
+        self.last_modify = datetime.now()
+        super().save(*args, **kwargs)
