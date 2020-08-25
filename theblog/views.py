3df80514aaa8664af600d98eefa1f5a972db261a
truongaxin123 <truongaxin123@gmail.com>
2020-08-25 23:08:39 +0700

    add comment form

--- theblog/views.py
+++ theblog/views.py
@@ -1,34 +1,7 @@
-from django.shortcuts import HttpResponse, render, get_object_or_404, Http404
-from .models import Article, Tag
-
-
-def get_next_or_prev(models: object, item: Article, direction: str = 'next') -> object:
-    getit = False
-    if direction == 'prev':
-        models = models.reverse()
-    for m in models:
-        if getit:
-            return m
-        if item == m:
-            getit = True
-    if getit:
-        # This would happen when the last
-        # item made getit True
-        return models[0]
-    return False
-
-
-def get_object_with_view_highest(objects):
-    result = None
-    view = 0
-    for object in objects:
-        view_tem = object.view
-        if view_tem > view:
-            result = object
-    return result
-
-def home(request):
-    return render(request, 'home.html')
+from django.shortcuts import HttpResponse, render, get_object_or_404, Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import Article, Tag, get_object_with_view_highest, get_next_or_prev
+from .forms import CommentForm
 
 
 def detail_article(request, slug):
@@ -41,19 +14,27 @@ def detail_article(request, slug):
     next_article = get_next_or_prev(models, article)
     previous_article = get_next_or_prev(models, article, 'prev')
 
+    comment_form = CommentForm()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST, author=request.user, article=article)
+        if comment_form.is_valid():
+            comment_form.save()
+            return HttpResponseRedirect(request.path)
+
     context = {'article': article,
                'next_article': next_article,
                'previous_article': previous_article,
-               'tag_with_view_highest': tag_with_view_highest}
+               'tag_with_view_highest': tag_with_view_highest,
+               'comment_form': comment_form}
 
-    return render(request,'view_detail_article.html', context=context)
+    return render(request, 'theblog/view_detail_article.html', context=context)
 
 
 def detail_tag(request, tag_name):
     tag = get_object_or_404(Tag, tag_name=tag_name)
-    tag.view +=1
+    tag.view += 1
     tag.save()
-    return render(request,'view_detail_tag.html', context={'tag':tag})
+    return render(request, 'theblog/view_detail_tag.html', context={'tag': tag})
 
 
 def list_article(request):
@@ -65,21 +46,23 @@ def list_article(request):
         tag_name = 'all'
         tag_full_name = 'Tất cả'
     tag = AllTag()
-    return render(request, 'view_list_article.html', context={'tag': tag, 'articles':articles})
+    return render(request, 'theblog/view_list_article.html', context={'tag': tag, 'articles': articles})
 
 
 def list_tag(request):
     tags = Tag.objects.all()
-    return render(request, 'view_all_tag.html', context={'tags':tags})
+    return render(request, 'theblog/view_all_tag.html', context={'tags': tags})
 
 
 def list_article_with_tag(request, tag_name):
     tag = get_object_or_404(Tag, tag_name=tag_name)
     articles = tag.article_set.all()
-    return render(request, 'view_list_article.html', context={'tag': tag, 'articles':articles})
+    return render(request, 'theblog/view_list_article.html', context={'tag': tag, 'articles': articles})
 
-def joke(request):
-    return render(request, 'joke.html')
 
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'theblog/profile.html')
+
+
+def joke(request):
+    return render(request, 'theblog/joke.html')
