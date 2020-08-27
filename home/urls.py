02b8b5ddf99ec766cdbff9519681c9eeb0074f13
truongaxin123 <truongaxin123@gmail.com>
2020-08-27 23:57:06 +0700

    edit home/url

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('register/success', views.register_success, name='register_success'),
-    path('login/', auth_views.LoginView.as_view(template_name='home/login.html', extra_context={'next':'/'}), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/#sale'), name='logout')
 ]
 
